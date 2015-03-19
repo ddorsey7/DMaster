0a5f65871faab64310aa3046461252bf702af401
ddorsey7 <ddorsey7@gmu.edu>
2015-03-19 14:42:04 -0400

    fix

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@ window.onload = function()
 		}
 	
 		knocker = game.add.sprite(game.world.centerX,game.world.centerY, 'dude');
-		cop = game.add.sprite(game.rnd.integerInRange(100, 700), game.rnd.integerInRange(32, 200), 'police');
+		cop = game.add.sprite(game.rnd.integerInRange(100, 700), game.rnd.integerInRange(32, 200), 'cop');
 
 		game.physics.enable([knocker,balls], Phaser.Physics.ARCADE);
 
