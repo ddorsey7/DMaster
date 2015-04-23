c7922ad17369b7fce998bce0ee554d11c9b036fc
ddorsey7 <ddorsey7@gmu.edu>
2015-04-23 06:48:44 -0400

    launcher update 2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ window.onload = function()
 {   
     "use strict";
     
-	var game = new Phaser.Game(1600, 1200, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update});
+	var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update});
 	function preload() {
 
     //game.load.image('analog', 'assets/tests/fusia.png');
